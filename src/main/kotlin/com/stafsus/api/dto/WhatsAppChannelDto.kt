4d69973f0e33680de080c10294f289a13f60ce39
kiditz <kiditzbastara@gmail.com>
2021-09-13 01:06:45 +0700

    SW-29 #time 1m ketinggalan jancuk!

---
+++ src/main/kotlin/com/stafsus/api/dto/WhatsAppChannelDto.kt
@@ -0,0 +1,12 @@
+package com.stafsus.api.dto
+
+data class WhatsAppChannelDto(
+	val phone: String? = null,
+	var browserSession: String? = null,
+	var qrCode: String? = null,
+	var deviceId: String? = null,
+	val pushName: String? = null,
+	var isOnline: Boolean? = null,
+	var isActive: Boolean? = null,
+	var isPending: Boolean? = null,
+)
