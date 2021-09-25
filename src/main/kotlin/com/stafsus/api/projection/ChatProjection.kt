57bc253c2c94843cca28df2f545a64d743356078
kiditz <kiditzbastara@gmail.com>
2021-09-25 22:03:59 +0700

    SW-38 #time 10m add more data for projection

--- src/main/kotlin/com/stafsus/api/projection/ChatProjection.kt
+++ src/main/kotlin/com/stafsus/api/projection/ChatProjection.kt
@@ -1,6 +1,7 @@
 package com.stafsus.api.projection
 
 import org.springframework.beans.factory.annotation.Value
+import java.time.OffsetDateTime
 
 interface ChatProjection {
 	fun getId(): String
@@ -13,6 +14,9 @@ interface ChatProjection {
 	fun isReadOnly(): Boolean
 	fun isPinned(): Boolean
 	fun isMuted(): Boolean
+	fun getUnreadCount(): Long
+	fun getMuteExpiration(): OffsetDateTime
+
 	@Value("#{target.contact.picture}")
 	fun getPicture(): String
 }
