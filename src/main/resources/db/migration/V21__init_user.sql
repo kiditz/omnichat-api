75f9f85206f03e72229092cec410e3bf832ac64e
kiditz <kiditzbastara@gmail.com>
2021-12-22 22:49:09 +0700

    SW-77 #time 1h fix data initialization

--- src/main/resources/db/migration/V21__init_user.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user_authority VALUES (1, 'ADMIN');
-INSERT INTO user_authority VALUES (2, 'STAFF');
-INSERT INTO user_authority VALUES (3, 'SUPER_ADMIN');
-INSERT INTO user_authority VALUES (4, 'SUPERVISOR');
-SELECT pg_catalog.setval('public.user_authority_id_seq', 4, true);
